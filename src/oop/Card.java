1989d89ae768b87b88449a32e376dbc10f1ba28b
Grinyov <vgrinyov@gmail.com>
2015-07-15 17:08:55 +0300

    upd

---
+++ src/oop/Card.java
@@ -0,0 +1,23 @@
+package oop;
+
+/**
+ * Created by Grinyov Vitaliy on 15.07.15.
+ */
+public class Card {
+    // инициализированная константа экземпляра
+    public final int ID = (int)(Math.random() * 10_000_000);
+    // неинициализированная константа
+    public final long BANK_ID; // инициализация по умолчанию не производится!
+    // { BANK_ID = 11111111L; } // только один раз!!!
+    public Card (long id) {
+// инициализация в конструкторе
+        BANK_ID = id; // только один раз!!!
+    }
+    public final boolean checkRights(final int NUMBER) {
+        final int CODE = 72173394; // антишаблон: "Волшебное Число"
+// ID = 1; // ошибка компиляции!
+// NUMBER = 1; // ошибка компиляции!
+// CODE = 1; // ошибка компиляции!
+        return CODE == NUMBER + ID;
+    }
+}
